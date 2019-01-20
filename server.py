8c9c9f1f2beb87fafa211cac38813ec102d8e648
Eugene Prokopiev <enp@itx.ru>
2019-01-20 22:48:33 +0300

    use bgapi and filter event keys

--- server.py
+++ server.py
@@ -11,41 +11,50 @@ msisdn = None
 
 sockets = []
 
+def filter(input):
+    output = {}
+    for key in ['Event-Name','Job-Command','Job-UUID','Unique-ID','Caller-Logical-Direction','Caller-Caller-ID-Number','Caller-Destination-Number','Hangup-Cause']:
+            if key in input:
+                output[key] = input[key]
+    return output
+
+async def request(data):
+    global connection    
+    print('REQUEST  : '+data)
+    response = await connection.bgapi(data)
+    print('RESPONSE : '+json.dumps(filter(response)))
+
 async def commands(websocket, path):
     global msisdn
     sockets.append(websocket)
     while(loop.is_running()):
         message = await websocket.recv()
+        print('COMMAND  : '+message)
         command = json.loads(message)
-        print(command)
         if (command['action'] == 'call' and command['destination']):
-            connection.api('originate {origination_caller_id_number='+msisdn+'}sofia/gateway/mss/'+command['destination']+' &park()')
+            await request('originate {origination_caller_id_number='+msisdn+'}sofia/gateway/mss/'+command['destination']+' &park()')
 
 async def events(host, loop):
     global connection    
     while(loop.is_running()):
-        event = await connection.recv_event()
-        name = event['Event-Name']
-        uuid = event['Unique-ID']
-        caller = event['Caller-Caller-ID-Number']
-        called = event['Caller-Destination-Number']
-        event = caller+' => '+called+' : '+name
-        print(event)
+        event = filter(await connection.recv_event())
+        print('EVENT    : '+json.dumps(event))
         global sockets
         for socket in sockets:
-            await socket.send(event)
-        if name == 'CHANNEL_PARK':
-            connection.api('uuid_answer '+uuid)
-        elif name == 'CHANNEL_ANSWER':
-            connection.api('uuid_broadcast '+uuid+' playback::/opt/media/welcome.wav')
-        elif name == 'PLAYBACK_STOP':
-            connection.api('uuid_kill  '+uuid+' playback::/opt/media/welcome.wav')
+            await socket.send(json.dumps(event))
+        job = None
+        if event['Event-Name'] == 'CHANNEL_PARK':
+            await request('uuid_answer '+event['Unique-ID'])
+        elif event['Event-Name'] == 'CHANNEL_ANSWER':
+            await request('uuid_broadcast '+event['Unique-ID']+' playback::/opt/media/welcome.wav')
+        elif event['Event-Name'] == 'PLAYBACK_STOP':
+            await request('uuid_kill  '+event['Unique-ID']+' playback::/opt/media/welcome.wav')
 
 if len(sys.argv) == 2:
     msisdn = sys.argv[1]
     loop = asyncio.get_event_loop()    
     connection = get_connection('sipbox', loop=loop)
     connection.connect()
-    connection.subscribe(['CHANNEL_PARK','CHANNEL_ANSWER','PLAYBACK_START','PLAYBACK_STOP','CHANNEL_HANGUP'])    
+    connection.subscribe(['BACKGROUND_JOB','CHANNEL_PARK','CHANNEL_ANSWER','PLAYBACK_START','PLAYBACK_STOP','CHANNEL_HANGUP'])    
     loop.run_until_complete(websockets.serve(commands, 'localhost', 8765))
     loop.run_until_complete(events('sipbox',loop))
